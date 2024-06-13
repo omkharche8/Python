d11e649bc73d9711bd333c2c07c566780e5e9474
Om Kharche <orkharche@gmail.com>
2024-06-13 20:14:48 +0530

    12 June only

--- DSA/Arrays/Level2/common_in_3.py
+++ DSA/Arrays/Level2/common_in_3.py
@@ -4,7 +4,7 @@
 # Output: 20, 80
 
 def common(arr1, arr2, arr3):
-    return set(arr1) & set(arr2) & set(arr3)
+    return list(set(arr1) & set(arr2) & set(arr3))
 
 
 arr1 = [1, 5, 10, 20, 40, 80]
