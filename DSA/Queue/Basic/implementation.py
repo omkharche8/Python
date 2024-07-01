b07c9c1debc82111b8d088531747650e6f4a3c05
Om Kharche <orkharche@gmail.com>
2024-07-01 15:07:54 +0530

    deque doin

---
+++ DSA/Queue/Basic/implementation.py
@@ -0,0 +1,51 @@
+# Queue directly from collections
+from collections import deque
+
+q = deque()
+
+q.appendleft(5)
+q.appendleft(24)
+q.appendleft(10)
+
+
+# print(q.pop())
+
+
+class Queue:
+
+    def __init__(self):
+        self.buffer = deque()
+
+    def enqueue(self, val):
+        self.buffer.appendleft(val)
+
+    def dequeue(self):
+        return self.buffer.pop()
+
+    def is_empty(self):
+        return len(self.buffer) == 0
+
+    def size(self):
+        return len(self.buffer)
+
+
+pq = Queue()
+
+pq.enqueue({
+    'company': 'Wall Mart',
+    'timestamp': '15 apr, 11.01 AM',
+    'price': 131.10
+})
+pq.enqueue({
+    'company': 'Wall Mart',
+    'timestamp': '15 apr, 11.02 AM',
+    'price': 132
+})
+pq.enqueue({
+    'company': 'Wall Mart',
+    'timestamp': '15 apr, 11.03 AM',
+    'price': 135
+})
+
+# print(pq.buffer)
+print(pq.dequeue())
