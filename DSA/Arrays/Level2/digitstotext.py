29f1438535c2c5def0303a3b8399e2ae8a5ea2c9
Om Kharche <orkharche@gmail.com>
2024-07-12 17:03:18 +0530

    doing some sorting

---
+++ DSA/Arrays/Level2/digitstotext.py
@@ -0,0 +1,19 @@
+# Input: GEEKSFORGEEKS
+# Output: 4333355777733366677743333557777
+
+
+def digitstotext(string):
+    index = {
+        2:"abc",
+        3:"def",
+        4:"ghi",
+        5:"jkl",
+        6:"mno",
+        7:"pqrs",
+        8:"tuv",
+        9:"wxyz"
+    }
+    output = ""
+    for i in range(len(string)):
+        pass
+
