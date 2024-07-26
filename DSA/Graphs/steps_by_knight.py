37fbe83d106f7f0ed3abe028955d9488cda259ac
Om Kharche <orkharche@gmail.com>
2024-07-26 21:07:35 +0530

    cool

--- DSA/Graphs/steps_by_knight.py
+++ DSA/Graphs/steps_by_knight.py
@@ -13,40 +13,40 @@
 
 from collections import deque
 
-
 class Solution:
 
     # Function to check if a position is valid
     def isValid(self, x, y, N):
         return 0 <= x < N and 0 <= y < N
 
-        # Function to find out minimum steps Knight needs to reach target position.
-        def minStepToReachTarget(self, KnightPos, TargetPos, N):
-            directions = [
-                (-2, -1), (-1, -2), (1, -2), (2, -1),
-                (2, 1), (1, 2), (-1, 2), (-2, 1)
-            ]
+    directions = [
+        (-2, -1), (-1, -2), (1, -2), (2, -1),
+        (2, 1), (1, 2), (-1, 2), (-2, 1)
+    ]
 
-            board = [[0] * N for _ in range(N)]
+    # Function to find out minimum steps Knight needs to reach target position.
+    def minStepToReachTarget(self, KnightPos, TargetPos, N):
+        board = [[0] * N for _ in range(N)]
 
-            # Converting the indexes
-            KnightPos = (KnightPos[0] - 1, KnightPos[1] - 1)
-            TargetPos = (TargetPos[0] - 1, TargetPos[1] - 1)
+        # Converting the indexes
+        KnightPos = (KnightPos[0] - 1, KnightPos[1] - 1)
+        TargetPos = (TargetPos[0] - 1, TargetPos[1] - 1)
 
-            queue = deque([(KnightPos[0], KnightPos[1], 0)])
+        queue = deque([(KnightPos[0], KnightPos[1], 0)])
 
-            visited = set()
-            visited.add(KnightPos[0], KnightPos[1])
+        visited = set()
+        visited.add((KnightPos[0], KnightPos[1]))
 
-            while queue:
-                x, y, steps = queue.popleft()
+        while queue:
+            x, y, steps = queue.popleft()
 
-                if (x, y) == TargetPos:
-                    return steps
+            if (x, y) == TargetPos:
+                return steps
 
-                for dx, dy in directions:
-                    nx, ny = x + dx, y + dy
-                    if self.isValid(nx, ny, N) and (nx, ny) not in visited:
-                        visited.add(nx, ny)
-                        queue.append(nx, ny, steps + 1)
-            return -1
+            for dx, dy in self.directions:
+                nx, ny = x + dx, y + dy
+                if self.isValid(nx, ny, N) and (nx, ny) not in visited:
+                    visited.add((nx, ny))
+                    queue.append((nx, ny, steps + 1))
+
+        return -1
